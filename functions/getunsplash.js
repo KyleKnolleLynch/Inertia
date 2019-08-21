3656919ba083c08629cf739b0047bd93696a127f
Kyle Lynch <kylelynch104@gmail.com>
2019-08-21 11:22:44 -0500

    fix api queries

--- functions/getunsplash.js
+++ functions/getunsplash.js
@@ -6,18 +6,28 @@ exports.handler = function(event, context, callback) {
   let query;
 
   hours < 4
-    ? (query = 'night')
+    ? (query = '&query=nature+night')
     : hours < 12
-    ? (query = 'morning')
+    ? (query = '&query=nature+morning')
     : hours < 18
-    ? (query = 'afternoon')
+    ? (query = '&query=nature+afternoon')
     : hours < 24
-    ? (query = 'night')
-    : (query = 'nature');
+    ? (query = '&query=nature+night')
+    : (query = '&query=nature');
+
+  // if (hours < 4) {
+  //   query = '&query=nature+night';
+  // } else if (hours < 12) {
+  //   query = '&query=nature+morning';
+  // } else if (hours < 18) {
+  //   query = '&query=nature+afternoon';
+  // } else if (hours < 24) {
+  //   query = '&query=nature+night';
+  // }
 
   const { UNSPLASH_CLIENT_ID } = process.env;
   const BASE_URL = 'https://api.unsplash.com/photos/random';
-  const API_URL = `${BASE_URL}?client_id=${UNSPLASH_CLIENT_ID}&query=${query}`;
+  const API_URL = `${BASE_URL}?client_id=${UNSPLASH_CLIENT_ID}${query}`;
 
   const send = body => {
     callback(null, {
