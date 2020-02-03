7676a8974c9f0f563237ede3725ad4344b08e31f
Kyle Lynch <kylelynch104@gmail.com>
2020-02-03 16:10:28 -0600

    static cache

--- sw.js
+++ sw.js
@@ -1,34 +1,34 @@
-const staticCache = "site-static-v2";
-const dynamicCache = "site-dynamic-v2";
+const staticCache = 'site-static-v2';
+const dynamicCache = 'site-dynamic-v2';
 
 const assets = [
-  "/",
-  "/index.html",
-  "/app.js",
-  "/weather.js",
-  "/css/style.css",
-  "https://fonts.googleapis.com/css?family=Exo:300&display=swap",
-  "https://fonts.googleapis.com/css?family=EB+Garamond&display=swap",
-  "https://fonts.googleapis.com/css?family=Nanum+Gothic&display=swap",
-  "https://use.fontawesome.com/releases/v5.6.3/css/all.css",
-  "https://favqs.com/api/qotd",
-  "https://favqs.com/"
+  '/',
+  '/index.html',
+  '/app.js',
+  '/weather.js',
+  '/css/style.css',
+  'https://fonts.googleapis.com/css?family=Exo:300&display=swap',
+  'https://fonts.googleapis.com/css?family=EB+Garamond&display=swap',
+  'https://fonts.googleapis.com/css?family=Nanum+Gothic&display=swap',
+  'https://use.fontawesome.com/releases/v5.6.3/css/all.css',
+  'https://favqs.com/api/qotd',
+  'https://favqs.com/'
 ];
 
 //  install sw
-self.addEventListener("install", e => {
-  console.log("service worker installed");
+self.addEventListener('install', e => {
+  console.log('service worker installed');
   e.waitUntil(
     caches.open(staticCache).then(cache => {
-      console.log("caching shell assets");
+      console.log('caching shell assets');
       cache.addAll(assets);
     })
   );
 });
 
 //  activate event
-self.addEventListener("activate", e => {
-  console.log("service worker has been activated");
+self.addEventListener('activate', e => {
+  console.log('service worker has been activated');
   e.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(
@@ -40,21 +40,31 @@ self.addEventListener("activate", e => {
   );
 });
 
-self.addEventListener("fetch", e => {
-  console.log("fetch event ");
+// self.addEventListener("fetch", e => {
+//   console.log("fetch event ");
+//   e.respondWith(
+//     caches
+//     .match(e.request)
+//     .then(cacheRes => {
+//       return cacheRes
+//       || fetch(e.request).then(fetchRes => {
+//         return caches.open(dynamicCache).then(cache => {
+//           cache.put(e.request.url, fetchRes.clone());
+//           return fetchRes;
+//         })
+//       })
+//     })
+//   );
+// });
+
+self.addEventListener('fetch', e => {
+  console.log('fetch event');
   e.respondWith(
-    caches
-    .match(e.request)
-    .then(cacheRes => {
-      return cacheRes 
-      || fetch(e.request).then(fetchRes => {
-        return caches.open(dynamicCache).then(cache => {
-          cache.put(e.request.url, fetchRes.clone());
-          return fetchRes;
-        })
-      })
+    caches.match(e.request).then(cacheRes => {
+      if (cacheRes) {
+        return cacheRes;
+      }
+      return fetch(e.request);
     })
   );
 });
-
-
