e3a5305d062ca49429d7fbad5bac45795664be74
Kyle Lynch <kylelynch104@gmail.com>
2021-01-21 12:08:49 -0600

    update service worker

--- sw.js
+++ sw.js
@@ -1,45 +1,49 @@
-const staticCache = 'site-static-v1';
-const dynamicCache = 'site-dynamic-v1';
+const staticCache = 'site-static-v1'
+const dynamicCache = 'site-dynamic-v1'
 
 const assets = [
   '/',
   '/index.html',
   '/app.js',
   '/css/style.css',
+  '/css/line-awesome/css/line-awesome.min.css',
+  '/css/line-awesome/fonts/la-solid-900.woff2',
+  '/css/owf-css/owfont-regular.min.css',
   '/images/default-early-am.webp',
   '/images/default-morning.webp',
   '/images/default-night.webp',
   '/images/rox-park-noon.webp',
   '/images/icons/inertiaIcon_96.png',
-  '/css/fonts/Exo-Light.woff',
-  '/css/fonts/EBGaramond-VariableFont_wght.woff',
-  '/css/fonts/EBGaramond-Italic-VariableFont_wght.woff'
-];
+  '/css/fonts/Exo-Light.woff2',
+  '/css/fonts/EBGaramond-VariableFont_wght.woff2',
+  '/css/fonts/EBGaramond-Italic-VariableFont_wght.woff2',
+  '/css/fonts/owfont-regular.woff2',
+]
 
 //  install sw
 self.addEventListener('install', e => {
-  console.log('service worker installed');
+  console.log('service worker installed')
   e.waitUntil(
     caches.open(staticCache).then(cache => {
-      console.log('caching shell assets');
-      cache.addAll(assets);
+      console.log('caching shell assets')
+      cache.addAll(assets)
     })
-  );
-});
+  )
+})
 
 //  activate event
 self.addEventListener('activate', e => {
-  console.log('service worker has been activated');
+  console.log('service worker has been activated')
   e.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(
         keys
           .filter(key => key !== staticCache && key !== dynamicCache)
           .map(key => caches.delete(key))
-      );
+      )
     })
-  );
-});
+  )
+})
 
 // self.addEventListener("fetch", e => {
 //   console.log("fetch event ");
@@ -59,13 +63,13 @@ self.addEventListener('activate', e => {
 // });
 
 self.addEventListener('fetch', e => {
-  console.log('fetch event');
+  console.log('fetch event')
   e.respondWith(
     caches.match(e.request).then(cacheRes => {
       if (cacheRes) {
-        return cacheRes;
+        return cacheRes
       }
-      return fetch(e.request);
+      return fetch(e.request)
     })
-  );
-});
+  )
+})
