3228284ac8a434c1a87392a3a84b91bd5954103e
Kyle Lynch <kylelynch104@gmail.com>
2021-03-25 16:50:51 -0500

    grid to flex dir col- main bg

--- app.js
+++ app.js
@@ -492,7 +492,6 @@ document.querySelectorAll('input[type=checkbox]').forEach(box => {
 //  execute weather.js functions when DOM load is complete
 document.addEventListener('DOMContentLoaded', getLocation)
 
-
 //    Event Listeners   //
 user.addEventListener('click', clearUser)
 user.addEventListener('keydown', setUser)
